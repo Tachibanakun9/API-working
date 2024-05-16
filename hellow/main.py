a5f94b71b0f30470c6b47f68b03bf3d8f51bf48e
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-05-16 15:51:39 +0530

    Update main.py

--- hellow/main.py
+++ hellow/main.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.params import Body
+from typing import Optional
 
 app = FastAPI()
 
 
-class post(BaseModel):
+class Post(BaseModel):
     title: str
     content: str
     published: bool = True
     rating: optional[int] = None
  
-
+my_post = [{"title": "title of the post 1", "content": "content of the post 1", "id": 1}, {"title": "hi everyone", "content": "i like to play games", "id": 2}]
+#remember wherever we open and run our server this data will be removed so for this we are 
+#going to hard core it
 
 @app.get("/")
 def root():
@@ -19,11 +22,11 @@ def root():
 
 @app.get("/post")
 def get_post():
-    return {"data": "this is your post"}
+    return {"data": my_post}
 
 
-@app.post("/createpost")
-def create_post(post: post):
+@app.post("/post")
+def create_post(post: Post):
     print(post.published)
     print(post.rating)
     # if ever you need to convert a schema to a python dictionary 
